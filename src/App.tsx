7c393358f4d673c33f5e7e068c6e9ca4ec9c2344
Sandra Mastrogiacomo <sandramastrogiacomo@hotmail.com>
2025-04-07 11:22:40 -0300

    Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter,Route,Routes} from 'react-router-dom';
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
     return (
-        <Home />
+     <>
+
+         <BrowserRouter>
+            <Navbar />
+         <div className="min-h-[80vh]">
+            <Routes>
+           <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+    </Routes>
+    </div>
+      <Footer />
+     </BrowserRouter>
+    </>
+ 
     )
 }
 
